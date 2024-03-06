d334ffc6cbe9db23e728727102a75b74dbc10dc3
MADHAVPATIL1818 <162277063+MADHAVPATIL1818@users.noreply.github.com>
2024-03-06 13:14:28 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,12 @@
+class Program1
+{
+ public static void main(String[]args)
+ {
+   System.out.println("name-madhav mukund patil");
+   System.out.println("native place-mangalwedha");
+   System.out.println("branch-civil engineering");
+   System.out.println("yop-2023");
+ 
+ }
+
+}
